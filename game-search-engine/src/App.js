a2ffed577c6d05fd036982481c3cacae0f2119ef
Kenneth Serrano <ken_serrano@Hotmail.com>
2019-03-04 15:23:44 -0500

    Added Background image and started search box component.

--- game-search-engine/src/App.js
+++ game-search-engine/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import TheHeader from './TheHeader';
+import SearchBox from './SearchBox';
 
 class App extends Component {
   render() {
     return (
+      <div>
       <TheHeader></TheHeader>
+      <SearchBox></SearchBox>
+      </div>
     );
   }
 }
--- game-search-engine/src/TheHeader.js
+++ game-search-engine/src/TheHeader.js
@@ -5,7 +5,7 @@ export default class TheHeader extends Component {
         return (
             <div>
                 <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <a class="navbar-brand" href="#">Search Games</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
