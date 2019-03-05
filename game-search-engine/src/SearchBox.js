2130b13006fadde59bce0b4fdf1cbc630afd23c4
Kenneth Serrano <ken_serrano@Hotmail.com>
2019-03-04 23:58:16 -0500

    Added search bar, but not fixed yet

--- game-search-engine/src/SearchBox.js
+++ game-search-engine/src/SearchBox.js
@@ -4,7 +4,19 @@ export default class SearchBox extends Component {
     
     render() {
         return (
-            <div>Hi</div>
+            <div>
+                <div>
+                    {/*Fix the search box*/}
+                    <div class="container col-lg-12 wrap">
+                        <div class="search">
+                            <input type="text" class="searchTerm" placeholder="What are you looking for?" />
+                            <button type="submit" class="searchButton">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
