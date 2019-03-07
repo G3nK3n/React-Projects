3e4a5f479467b1a6b6a227f636bc54143c587e3e
Kenneth Serrano <ken_serrano@Hotmail.com>
2019-03-07 17:00:07 -0500

    Added search box container, ading soon search bar.

--- game-search-engine/src/SearchBox.js
+++ game-search-engine/src/SearchBox.js
@@ -5,8 +5,8 @@ export default class SearchBox extends Component {
     render() {
         return (
             <div>
-                <div>
-                    {/*Fix the search box*/}
+                <div class="container col-lg-5 searchBoxContainer">
+                    {/*Fix the search box
                     <div class="container col-lg-12 wrap">
                         <div class="search">
                             <input type="text" class="searchTerm" placeholder="What are you looking for?" />
@@ -14,7 +14,7 @@ export default class SearchBox extends Component {
                                 <i class="fa fa-search"></i>
                             </button>
                         </div>
-                    </div>
+                    </div>*/}
                 </div>
             </div>
         );
