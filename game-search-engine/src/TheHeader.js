5f0fa8ab4b6ab90d27a2fa17a826ecd8379bfe87
Kenneth Serrano <ken_serrano@Hotmail.com>
2019-05-01 23:20:37 -0400

    Centered title, fixed search box and added a little styling

--- game-search-engine/src/TheHeader.js
+++ game-search-engine/src/TheHeader.js
@@ -4,18 +4,18 @@ export default class TheHeader extends Component {
     render() {
         return (
             <div>
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand" href="#">Search Games</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <a className="navbar-brand" href="#">Search Games</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Login</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Login</a>
                             </li>
                             {/*<li class="nav-item">
                                 <a class="nav-link" href="#">Pricing</a>
