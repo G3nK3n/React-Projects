ff37f5ea4339e5eddbb1c3df5069abc7c8c9d8f2
Kenneth Serrano <ken_serrano@Hotmail.com>
2019-11-04 22:37:30 -0500

    CSS Styling, JXS and regular, event handlers

--- react-practice-1/src/App.js
+++ react-practice-1/src/App.js
@@ -12,25 +12,45 @@ class App extends Component {
     ]
   }
 
-  switchMethodHandler = () => {
+  switchMethodHandler = (newName) => {
     //console.log('It is clicked!');
     // DONT DO THIS this.state.persons[0].name = "Other name";
     this.setState({
       persons: [
-      { name: 'NewName' },
+      { name: newName },
       { name: 'Kev' }, 
       { name: 'wtv' }
       ] 
     })
   }
 
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+      { name: 'Ken' },
+      { name: event.target.value }, 
+      { name: 'wtv' }
+      ] 
+    })
+  }
+
   render() {
+
+  const style = {
+    //JSX style of using styles.
+    backgroundColor: 'white', 
+    font: 'inherit',
+    border: '1x solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
+
     return (
       <div className="App">
         <h1>Hi! lol</h1>
-        <button onClick={this.switchMethodHandler}>Some Button</button>
+        <button style={style} onClick={() => this.switchMethodHandler("lol")}>Some Button</button>
         <Person name={this.state.persons[0].name}>Is this working?</Person>
-        <Person name={this.state.persons[1].name}>Now?</Person>
+        <Person change={this.nameChangeHandler} click={this.switchMethodHandler.bind(this, "LOL")} name={this.state.persons[1].name}>Now?</Person>
         <Person name={this.state.persons[2].name}>How about now?</Person>
       </div>
     );
--- react-practice-1/src/Person/Person.js
+++ react-practice-1/src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
-            <p>I am {props.name} and I am {Math.floor(Math.random() * 30)} years old</p>
+        <div className="Person">
+            <p onClick={props.click}>I am {props.name} and I am {Math.floor(Math.random() * 30)} years old</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name}/>
         </div>
     )
 };
