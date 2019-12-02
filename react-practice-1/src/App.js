91099fd2ee82cd347b376f3b69de86cdbf9bbc58
Kenneth Serrano <ken_serrano@Hotmail.com>
2019-12-01 23:03:38 -0500

    Added method to modify the name of each Person component, intro to splice and copying states

--- react-practice-1/src/App.js
+++ react-practice-1/src/App.js
@@ -21,14 +21,30 @@ class App extends Component {
     this.setState({persons: persons});
   }
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-      { name: 'Ken' },
-      { name: event.target.value }, 
-      { name: 'wtv' }
-      ] 
-    })
+  nameChangeHandler = (event, id) => {
+    //Checks and sees if the id matches with the one that is being changed.
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    //This gets a reference of the state with the index of the one we were looking for.
+    //Note that we did the spread operator to get a copy of the state, to not work directly 
+    //on the states.
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    //This changes the value of the event(textbox)
+    person.name = event.target.value;
+
+    //Gets a copy of the whole state, 
+    //then set the updated object to the state that is updating.
+    //You should do this to avoid working with the state directly, for security purposes.
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    //This will update the state to the changes provided above.
+    this.setState({persons: persons});
   }
 
   togglePersonHandler = () => {
@@ -56,7 +72,7 @@ class App extends Component {
         {this.state.persons.map((thePerson, index) => {
           //This outputs all the components using map functions
           //Key props identifies each component uniquely
-          return <Person name={thePerson.name} click={() => this.deletePersonHandler(index)} key={thePerson.id} />
+          return <Person name={thePerson.name} click={() => this.deletePersonHandler(index)} key={thePerson.id} change={(event) => this.nameChangeHandler(event, thePerson.id )}/>
         })}
       </div>
     )
