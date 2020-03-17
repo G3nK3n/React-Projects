5de9fc514e64b8e10429996e1aa21ea192a22e7f
Kenneth Serrano <ken_serrano@Hotmail.com>
2020-03-16 23:26:36 -0400

    Added bootstrap library

--- personal_website/src/AllComponents/Header.js
+++ personal_website/src/AllComponents/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const headerComponent = () => {
     return(
         <div className="header">
+         {/*
             <a href="#">
                 <img className="homeIcon" src="https://i.pinimg.com/236x/da/e0/92/dae092a545e4c09714ee9842ab15dc10.jpg" alt="home_icon" />
             </a>
@@ -12,6 +14,7 @@ const headerComponent = () => {
                 <a className="linkDisplay" href="#">About</a>
                 <a className="linkDisplay" href="#">Contact</a>
             </div>
+         */}
         </div>
     );
 }
