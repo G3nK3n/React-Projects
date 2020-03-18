9b34b828040d2fa41f4d6e315b6aa61820a0b532
Kenneth Serrano <ken_serrano@Hotmail.com>
2020-03-17 23:54:23 -0400

    Tested Bootstrap Header

--- personal_website/src/AllComponents/Header.js
+++ personal_website/src/AllComponents/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import '../App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; //Need this for Bootstrap as well
 
-const headerComponent = () => {
+//Bootstrap Imports
+import Navbar from 'react-bootstrap/Nav';
+
+const headerComponent = (props) => {
     return(
         <div className="header">
+         <Navbar className="NavBar">
+            <Navbar.Item>
+                <Navbar.Link className="NavBarLink" href="#"> About </Navbar.Link>
+            </Navbar.Item>
+            <Navbar.Item>
+                <Navbar.Link className="NavBarLink" href="#"> Contact </Navbar.Link>
+            </Navbar.Item>
+            <Navbar.Item>
+                <Navbar.Link className="NavBarLink" href="#"> Portfolio </Navbar.Link>
+            </Navbar.Item>
+         </Navbar>
          {/*
             <a href="#">
                 <img className="homeIcon" src="https://i.pinimg.com/236x/da/e0/92/dae092a545e4c09714ee9842ab15dc10.jpg" alt="home_icon" />
