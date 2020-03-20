6fd70ec8fdd247830da8a49acc73b0fba238e6c9
Kenneth Serrano <ken_serrano@Hotmail.com>
2020-03-20 00:09:48 -0400

    Added About Component. Created Container for About Me section.

--- personal_website/src/AllComponents/About.js
+++ personal_website/src/AllComponents/About.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; //Need this for Bootstrap as well
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 
 const aboutComponent = (props) => {
     return(
-        <div>
-            <p>This is the about component</p>
+        <div>  
+            <Container className="AboutDiv">
+                <Row>
+                    <Col sm={12}>
+                        <h1>About</h1>
+                    </Col>
+                    <Col sm={5}>
+                        <p>Hi</p>
+                        {/*MAKE LIST FOR ABOUT ME*/}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
--- personal_website/src/App.js
+++ personal_website/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return(
       <div>
          <Header />
-    {/*<About />*/}
+         <About />
       </div>
 
     );
