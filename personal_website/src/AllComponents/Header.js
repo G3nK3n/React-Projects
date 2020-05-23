fe46a163e5bebce6b3f0bcce7f6ce8b840271a20
Ken <ken_serrano@Hotmail.com>
2020-05-22 23:58:39 -0400

    Fixed Responsive Header

--- personal_website/src/AllComponents/Header.js
+++ personal_website/src/AllComponents/Header.js
@@ -3,13 +3,13 @@ import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; //Need this for Bootstrap as well
 
 //Bootstrap Imports
-import Navbar from 'react-bootstrap/Nav';
+import Nav from 'react-bootstrap/Nav';
 import NavbarBrand from 'react-bootstrap/NavbarBrand';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 //Used for Responsive behavior
-import Navbar2 from 'react-bootstrap/Navbar';
+import Navbar from 'react-bootstrap/Navbar';
 
 const headerComponent = (props) => {
     
@@ -18,7 +18,7 @@ const headerComponent = (props) => {
     return(
         <div className="header">
          <Container fluid>
-         <Navbar className="wholeNav">
+         <Nav className="wholeNav">
             <Row>
                 <Col sm={3} md={3} lg={2} className="d-none d-sm-block" > 
                     <NavbarBrand onClick={props.clickHome}className="NavBarBrand" href="#"> KEN SERRANO </NavbarBrand>
@@ -26,71 +26,60 @@ const headerComponent = (props) => {
 
                 <Col sm={9} md={9} lg={10} className="d-none d-sm-block">
                     <div className="NavBarItems">
-                        <Navbar.Item>
-                            <Navbar.Link onClick={props.clickResume} className="NavBarLink" href="#"> Resume </Navbar.Link>
-                        </Navbar.Item>
+                        <Nav.Item>
+                            <Nav.Link onClick={props.clickResume} className="NavBarLink" href="#"> Resume </Nav.Link>
+                        </Nav.Item>
                         <vl className="someVerticalLine2"/>
-                        <Navbar.Item>
-                            <Navbar.Link onClick={props.clickProject} className="NavBarLink" href="#"> Projects </Navbar.Link>
-                        </Navbar.Item>
+                        <Nav.Item>
+                            <Nav.Link onClick={props.clickProject} className="NavBarLink" href="#"> Projects </Nav.Link>
+                        </Nav.Item>
                         <vl className="someVerticalLine2"/>
-                        <Navbar.Item>
-                            <Navbar.Link onClick={props.clickPhotog} className="NavBarLink" href="#"> Photography </Navbar.Link>
-                        </Navbar.Item>
+                        <Nav.Item>
+                            <Nav.Link onClick={props.clickPhotog} className="NavBarLink" href="#"> Photography </Nav.Link>
+                        </Nav.Item>
                         <vl className="someVerticalLine2"/>
-                        <Navbar.Item>
-                            <Navbar.Link className="NavBarLink" onClick={props.clickAbout} href="#"> About </Navbar.Link>
-                        </Navbar.Item>
+                        <Nav.Item>
+                            <Nav.Link className="NavBarLink" onClick={props.clickAbout} href="#"> About </Nav.Link>
+                        </Nav.Item>
                         <vl className="someVerticalLine2"/>
-                        <Navbar.Item>
-                            <Navbar.Link onClick={props.clickContact} className="NavBarLink" href="#"> Contact </Navbar.Link>
-                        </Navbar.Item>
-                    </div>
-                </Col>
-                
-
-                {/* Added this to practice Bootstrap with react.
-                    This translates to only show in XS, while not in SM 
-                    Refer to the Bootstrap V4 website for more info */}
-                <Col xs={4} className="d-block d-sm-none" >
-                    <div className="navbarBrand">
-                        <Navbar2.Brand onClick={props.clickHome}className="NavBarBrand" href="#"> KEN SERRANO </Navbar2.Brand>
+                        <Nav.Item>
+                            <Nav.Link onClick={props.clickContact} className="NavBarLink" href="#"> Contact </Nav.Link>
+                        </Nav.Item>
                     </div>
                 </Col>
 
                 {/* RESPONSIVE HEADER */}
-                <Col xs={8} className="d-block d-sm-none">
-                    
-                    
-                    <Navbar2 expand="lg" variant="dark">
-                        <Navbar2.Toggle aria-controls="responsive-navbar-nav" />
-                        <Navbar2.Collapse id="responsive-navbar-nav">
-                        {/* <div className="NavBarItems"> */}
-                            <Navbar.Item>
-                                <Navbar.Link onClick={props.clickResume} className="NavBarLink" href="#"> Resume </Navbar.Link>
-                            </Navbar.Item>
+                <Col xs={12} className="d-block d-sm-none">
+                    <Navbar expand="lg" variant="dark">
+                    <Navbar.Brand onClick={props.clickHome} className="NavBarBrand"  href="#"> KEN SERRANO </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                        <div className="NavBarItems">
+                            <Nav.Item>
+                                <Nav.Link onClick={props.clickResume} className="NavBarLink" href="#"> Resume </Nav.Link>
+                            </Nav.Item>
                             <vl className="someVerticalLine2"/>
-                            <Navbar.Item>
-                                <Navbar.Link onClick={props.clickProject} className="NavBarLink" href="#"> Projects </Navbar.Link>
-                            </Navbar.Item>
+                            <Nav.Item>
+                                <Nav.Link onClick={props.clickProject} className="NavBarLink" href="#"> Projects </Nav.Link>
+                            </Nav.Item>
                             <vl className="someVerticalLine2"/>
-                            <Navbar.Item>
-                                <Navbar.Link onClick={props.clickPhotog} className="NavBarLink" href="#"> Photography </Navbar.Link>
-                            </Navbar.Item>
+                            <Nav.Item>
+                                <Nav.Link onClick={props.clickPhotog} className="NavBarLink" href="#"> Photography </Nav.Link>
+                            </Nav.Item>
                             <vl className="someVerticalLine2"/>
-                            <Navbar.Item>
-                                <Navbar.Link className="NavBarLink" onClick={props.clickAbout} href="#"> About </Navbar.Link>
-                            </Navbar.Item>
+                            <Nav.Item>
+                                <Nav.Link className="NavBarLink" onClick={props.clickAbout} href="#"> About </Nav.Link>
+                            </Nav.Item>
                             <vl className="someVerticalLine2"/>
-                            <Navbar.Item>
-                                <Navbar.Link onClick={props.clickContact} className="NavBarLink" href="#"> Contact </Navbar.Link>
-                            </Navbar.Item>
-                        {/* </div> */}
-                        </Navbar2.Collapse>
-                    </Navbar2>
+                            <Nav.Item>
+                                <Nav.Link onClick={props.clickContact} className="NavBarLink" href="#"> Contact </Nav.Link>
+                            </Nav.Item>
+                        </div>   
+                        </Navbar.Collapse>
+                    </Navbar>
                 </Col>
             </Row>
-         </Navbar>
+         </Nav>
          </Container>
         </div>
     );
