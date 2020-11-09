df44fc42f4de80d46da504b2e6ed558130ba90ed
Ken <ken_serrano@Hotmail.com>
2020-11-08 23:53:48 -0500

    Fixed Header

--- recreation-2/src/Components/Home/Header/Header.js
+++ recreation-2/src/Components/Home/Header/Header.js
@@ -12,18 +12,26 @@ import logo from '../../../logo.png';
 
 
 const header = () => (
-    <div className={classes.Header}>
-        <Container>
-            <Navbar >
-                <a href="javascript:void(0)" ><img src={logo} /></a>
-                <Nav>
-                    <Nav.Link>Home</Nav.Link>
-                    <Nav.Link>About</Nav.Link>
-                    <Nav.Link>Projects</Nav.Link>
-                    <Nav.Link>Blog</Nav.Link>
-                    <Nav.Link>Contact</Nav.Link>
-                </Nav>
-            </Navbar>
+    <div>
+        <Container className={classes.Header}>
+            <Row>
+                <Col md={4}>
+                    <Navbar>
+                        <a href="#" ><img src={logo} /></a>   
+                    </Navbar>
+                </Col>
+                <Col md={6}>
+                    <div>
+                        <Nav>
+                            <Nav.Link>Home</Nav.Link>
+                            <Nav.Link>About</Nav.Link>
+                            <Nav.Link>Projects</Nav.Link>
+                            <Nav.Link>Blog</Nav.Link>
+                            <Nav.Link>Contact</Nav.Link>
+                        </Nav>
+                    </div>
+                </Col>
+            </Row>
         </Container>
     </div>
 )
