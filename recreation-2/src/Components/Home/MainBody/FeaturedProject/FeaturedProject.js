6fff3334f28e743093a74daf11f166da9bbd121e
Ken <ken_serrano@Hotmail.com>
2020-11-13 00:59:16 -0500

    Fixed MainBody with FeaturedProject Component

---
+++ recreation-2/src/Components/Home/MainBody/FeaturedProject/FeaturedProject.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Img1 from '../../../../project-image1.jpg';
+import Img2 from '../../../../project-image2.jpg';
+import Img3 from '../../../../project-image3.jpg';
+import Img4 from '../../../../project-image4.jpg';
+import TransparentBack from '../../../../bg-transparent1.png';
+import Row from 'react-bootstrap/Row';
+
+
+import classes from '../FeaturedProject/FeaturedProject.module.css';
+
+const features = () => (
+    <div className={classes.Featured}>
+        <div className={classes.InnerDiv}>        
+            <h3>Featured Project</h3>
+        </div>
+        <ul>
+            <li>
+                <img src={Img1} />
+            </li>
+            <li>
+                <img src={Img2} />
+            </li>
+            <li>
+                <img src={Img3} />
+            </li>
+            <li>
+                <img src={Img4} />
+            </li>
+        </ul>
+    </div>
+)
+
+export default features;
--- recreation-2/src/Components/Home/MainBody/MainBody.js
+++ recreation-2/src/Components/Home/MainBody/MainBody.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from '../MainBody/MainBody.module.css';
+import Featured from './FeaturedProject/FeaturedProject';
 
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
@@ -9,12 +10,13 @@ import Img1 from '../../../satellite.png';
 const mainbody = () => (
     <div className={classes.BodyDiv}>
         <Container className={classes.MainBody}>
-            <div>
+            <div className={classes.thisDiv}>
                 <h1>Soyuz Tma-M</h1>
                 <h2>Spacecraft</h2>
                 <Button variant="dark">Read More</Button>
             </div>
             <img src={Img1} />
+            <Featured />
         </Container>
     </div>
 );
