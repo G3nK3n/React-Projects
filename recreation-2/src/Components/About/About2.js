daa309ba01e424d72e0afd3d4fb3ea409dd338cd
Ken <ken_serrano@Hotmail.com>
2020-12-06 21:45:26 -0500

    Finish FirstRow of About2, started second Row

--- recreation-2/src/Components/About/About2.js
+++ recreation-2/src/Components/About/About2.js
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Img1 from '../../earth-satellite.jpg';
+import Img2 from '../../space-shuttle.png';
 import classes from '../About/About2.module.css';
 
 
@@ -11,7 +12,7 @@ const about2 = () => (
         <div className={classes.About2FirstRow}>
             <Container>
                 <Row>
-                    <Col lg={8}>
+                    <Col md={6} lg={7} className="d-none d-lg-block d-xl-block">
                         <div>
                             <h2>We have more templates for you</h2>
                             <p>
@@ -22,10 +23,40 @@ const about2 = () => (
                             </p>
                         </div>
                     </Col>
-                    <Col lg={4}>
+                    <Col md={6} lg={5} className="d-none d-lg-block d-xl-block">
                         <div>
                             <img src={Img1} />
                         </div>
+                    </Col>
+                    
+                    {/* Medium size About2 section */}
+                    <Col className="d-block d-sm-block d-md-block d-lg-none">
+                        <div className={classes.About2FirstRowResp}>
+                            <img src={Img1} />
+                        </div>
+                        <div>
+                            <h2>We have more templates for you</h2>
+                            <p>
+                                Looking for more templates? Just browse through all our Free Website Templates and find what you’re looking for. 
+                                But if you don’t find any website template you can use, you can try our Free Web Design service and tell us all about it. 
+                                Maybe you’re looking for something different, something special. 
+                                And we love the challenge of doing something different and something special.
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <div>
+                            <img src={Img2} />
+                        </div>                   
+                    </Col>
+                    <Col>
+                    
                     </Col>
                 </Row>
             </Container>
