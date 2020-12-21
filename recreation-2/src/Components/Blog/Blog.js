1d8a707bee62719f7b92436027ac413c8a70365b
Ken <ken_serrano@Hotmail.com>
2020-12-20 21:42:53 -0500

    Started second column

--- recreation-2/src/Components/Blog/Blog.js
+++ recreation-2/src/Components/Blog/Blog.js
@@ -4,6 +4,9 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
 import classes from '../Blog/Blog.module.css';
+import Img1 from '../../moon-satellite.jpg';
+import Img2 from '../../alien-life.jpg';
+import Img3 from '../../galaxy.jpg';
 
 const blogpage = () => (
     <div className={classes.BlogPage}>
@@ -16,14 +19,42 @@ const blogpage = () => (
                 </Col>
             </Row>
             <Row>
-                <Col>
+                <Col xl={7}>
                     <div className={classes.Col1}>
                         <h1>Col 1</h1>
                     </div>
                 </Col>
-                <Col>
+                <Col xl={5}>
                     <div className={classes.Col2}>
-                        <h1>Col 2</h1>
+                        <div>
+                            <h1>Featured Post</h1>
+                            <img src={Img1} />
+                            <h2>Soyuz TMA-M</h2>
+                            <p>February 6, 2023</p>
+                        </div>
+                        <div>
+                            <h1>Recent Post</h1>
+                            <ul>
+                                <li>
+                                    <div>
+                                        <img src={Img2} />
+                                    </div>
+                                    <div>
+                                        <h2>Alien Life</h2>
+                                        <p>February 3, 2023</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div>
+                                        <img src={Img3} />
+                                    </div>
+                                    <div>
+                                        <h2>The Galaxy</h2>
+                                        <p>February 1, 2023</p>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </Col>
             </Row>
