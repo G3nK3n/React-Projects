600f7cfc25376fd404243932c9d574769f2e69c3
Ken <ken_serrano@Hotmail.com>
2021-01-12 23:38:59 -0500

    Added personal-website-ver2 project

---
+++ personal-website-ver2/src/Components/Layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classes from '../Components/Layout.module.css';
+
+class Layout extends Component {
+    render() {
+        return(
+            <div className={classes.BackgroundImage}>
+                
+            </div>
+        );
+    }
+}
+
+export default Layout;
